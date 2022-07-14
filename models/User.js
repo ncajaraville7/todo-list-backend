db3d9166aca3e1a0d80407e1c34291d94cc5e623
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-07-14 11:59:59 -0300

    add user model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    index: { unique: true },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

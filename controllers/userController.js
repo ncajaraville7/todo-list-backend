1b14aafb7a0544d07a4165648c1dd25c1bac942c
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-07-14 15:05:46 -0300

    add user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,4 +29,27 @@ const userRegister = async (req, res) => {
   }
 };
 
-export { userRegister };
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (!userExist) {
+    const error = new Error('El usuario no se encuentra registrado');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (password.length < 6) {
+    const error = new Error('La contraseña debe tener al menos 6 caracteres');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (password === userExist.password) {
+    console.log(req.body);
+  } else {
+    const error = new Error('Contraseña invalida');
+    return res.status(404).json({ msg: error.message });
+  }
+};
+
+export { userRegister, userLogin };

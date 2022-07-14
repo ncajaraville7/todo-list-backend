b57f9ccc30e24c11afcda2380ac8a486f288cee4
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-07-14 11:20:29 -0300

    Connect MONGODB

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import connectDB from './config/db.js';
+
 const app = express();
+
 dotenv.config();
-app.listen(process.env.PORT);
-console.log('servidor ejecutandose en el puerto ' + process.env.PORT);
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT);
+console.log('Servidor ejecutandose en el puerto ' + PORT);

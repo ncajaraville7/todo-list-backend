dd9d428bb4d7d397cb317f58fd7fb1c763505e94
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-07-14 11:34:11 -0300

    Add routes

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
 
+import userRoutes from './routes/userRoutes.js';
+import tasksRouter from './routes/tasksRouter.js';
+
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 connectDB();
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', tasksRouter);
 
 const PORT = process.env.PORT || 4000;
 
